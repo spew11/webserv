4977f4c7b1cab9bd269a1a711edce595e6e4179f
izone00 <woojin5555@gmail.com>
2023-07-31 21:19:29 +0900

    Comment: ConfigParser 주석 추가

--- srcs/ConfigParser.cpp
+++ srcs/ConfigParser.cpp
@@ -22,26 +22,27 @@ Config * ConfigParser::get( void )
     return NULL;
 }
 
+// 인자로 들어온 tree의 sub트리를 만든다.
 void ConfigParser::setTree( DerivTree * tree )
 {
     struct Derivative deriv;
 
-    while (getDeriv(deriv))
+    while (getDeriv(deriv))// token을 연결한 지시어(deriv)를 가져온다.
     {
         if (deriv.arg.size() == 0)
             continue;
 
-        switch (deriv.delim)
+        switch (deriv.delim) // 지시어 구분자(delim)
         {
-        case ';':
+        case ';': // 단순히 sub트리를 만든다.
             tree->addSubTree(DerivTree(deriv));
             break;
-        case '{':
+        case '{': // sub트리를 만들고 그 sub트리를 기준으로 함수를 재귀적으로 실행
             DerivTree * subTree;
             subTree = tree->addSubTree(DerivTree(deriv));
             setTree(subTree);
             break;
-        case '}':
+        case '}': // 트리가 완성됬으므로 함수를 끝낸다.
             return;
         }
     }
@@ -76,6 +77,7 @@ void ConfigParser::tokenize( const string & file, list<string> & list )
 
     string token;
 
+    // 화이트 스페이스를 기준으로 잘라준다.
     while (iss >> token)
     {
         list.push_back(token);
@@ -94,31 +96,38 @@ bool ConfigParser::getDeriv( Derivative & deriv )
 
     list<string>::iterator it = tokenList.begin();
 
+    // 구분자(delim)이 나올때 까지 vector에 push한다.
     while(it != tokenList.end())
     {
         string token = *it;
 
         size_t idx;
+        // token이 구분자를 가지는지 확인
         if (((idx = token.find(';')) != string::npos)
          || ((idx = token.find('{')) != string::npos)
          || ((idx = token.find('}')) != string::npos))
         {
+            // 구분자 이전의 substr을 arg에 넣어준다.
             deriv.arg.push_back(token.substr(0, idx));
 
+            // 이때 구분자가 token의 중간에 있다면 token을 구분자 기준으로 잘라 새로 초기화한다.
             if (idx == token.size() - 1)
                 it++;
             else
                 *it = token.substr(idx + 1);
             
+            // 지시어(deriv)의 변수를 초기화
             deriv.name = deriv.arg[0];
             deriv.delim = token[idx];
 
             break;
         }
+
         deriv.arg.push_back(token);
         it++;
     }
 
+    // 사용된 토큰은 삭제한다.
     if (tokenList.begin() != it)
         tokenList.erase(tokenList.begin(), it);
 
