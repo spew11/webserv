b0ae56cfad648ebb8587eb5a101da5afa96620bf
USER2 <user2@AD01937884.local>
2023-08-05 23:47:57 +0900

    Rename: EnvironmentValues -> WebservValues

--- include/LocationConfig.hpp
+++ include/LocationConfig.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <Modules.hpp>
-#include <EnvironmentValues.hpp>
+#include <WebservValues.hpp>
 
 class LocationConfig
 {
@@ -31,7 +31,7 @@ public:
     int  getClientMaxBodySize( void ) const;
     const vector<string> & getAcceptMethods( void ) const;
 
-    char ** getCgiParams( const EnvironmentValues & ) const;
+    char ** getCgiParams( const WebservValues & ) const;
 
     void print() const
     {
