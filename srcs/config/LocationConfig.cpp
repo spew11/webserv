303e1b888a5b4fb6e3fed7ac02cf1cf3395c9eb5
woojeong <woojin5555@gamil.com>
2023-08-11 00:20:41 +0900

    Fix: Bug: 모듈 없거나 LocConfMap 비었을시 에러 처리

--- srcs/config/LocationConfig.cpp
+++ srcs/config/LocationConfig.cpp
@@ -86,11 +86,14 @@ const string & LocationConfig::getType( const string & scriptName ) const
 {
 	static const string defaultType = "text/html";// 나중에 defaultType모듈로 변경
 
+	if (typesMod == NULL)
+		return defaultType;
+
 	size_t dotIdx = scriptName.find_last_of('.');
 
-	if (dotIdx != string::npos)
+	if (dotIdx != string::npos && dotIdx != scriptName.size() - 1)
 	{
-		string extension = scriptName.substr(dotIdx);
+		string extension = scriptName.substr(dotIdx + 1);
 
 		map<string, string>::const_iterator it = typesMod->getTypesMap().find(extension);
 
