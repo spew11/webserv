bb312062e65a23733eeeba1855d42e622254fcf9
soyeong-lee <soylee@student.42seoul.kr>
2023-08-17 18:23:00 +0900

    Fix: print cgi_env in test.py

--- html/test.py
+++ html/test.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-print('Hello World!')
+print("Content-Type: text/html;charset=UTF-8\n")
+
+import sys
+import os
+
+print("<h1>body</h1>")
+print(sys.stdin.read())
+
+print("<h1>env</h1>")
+env = os.environ.items()
+line = []
+for key, value in env:
+    line.append("({}, {})".format(key, value))
+print("<br>".join(line))
